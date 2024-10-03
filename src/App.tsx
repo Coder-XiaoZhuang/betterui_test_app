35776c2f25fc84bdd44777ee62ddbbaa214a09e2
better <2947028824@qq.com>
2024-10-03 23:28:44 +0800

    feat: 新增betterui测试仓库

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@zhuangjiaqing/betterui/dist/index.css';
+import { BetterButton } from '@zhuangjiaqing/betterui';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BetterButton btnType='primary'>Hello World</BetterButton>
     </div>
   );
 }
